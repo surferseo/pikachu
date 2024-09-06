2f612b81847cab7a528f7429b39ae5ee618e788d
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2024-09-06 09:21:04 +0200

    fix: avoid loading torch when not needed

--- pikachu/cuda_utils.py
+++ pikachu/cuda_utils.py
@@ -1,8 +1,8 @@
 from pikachu.api_utils import is_server
-import torch
 
 
 def assert_cuda_is_available():
+    import torch
 
     if is_server():
         assert torch.cuda.is_available()
@@ -13,10 +13,13 @@ def device():
     We require of server to have CUDA.
     This way we avoid running on CPU when CUDA is not available, what happens frequently due to https://github.com/NVIDIA/nvidia-docker/issues/1671.
     """
+    import torch
 
     use_cuda = is_server() or torch.cuda.is_available()
     return "cuda" if use_cuda else "cpu"
 
 
 def dtype():
+    import torch
+
     return torch.float16 if device() == "cuda" else torch.float32
